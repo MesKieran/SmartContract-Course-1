c47f6c7c761298e62dbd5ae36bd5655ad78b6fc4
"MesKieran" <"979714336@qq.com">
2022-06-30 09:43:33 +0800

    day4

---
+++ contracts/contract_address_balance.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Deposit{
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+    receive() external payable{
+
+    }
+    fallback() external payable{
+
+    }
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+    // transfer ether to contract
+    function sendEther() public payable{
+        uint x;
+        x++;
+    }
+    function transferEther(address payable recipient, uint amount) public returns(bool){
+        require(owner == msg.sender, "Transder failed, you are not the owner!");
+        if (amount <= getBalance()){
+            recipient.transfer(amount);
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
